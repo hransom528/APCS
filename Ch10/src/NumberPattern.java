4193dc37675fe39559164b5bc35c15bdac7ee2b9
Harris Ransom <hransom528@gmail.com>
2020-01-17 09:14:00 -0500

    Created Ch10 and labs
    -New Ch10 project
    -NumberPattern
    -PhotoLineups

---
+++ Ch10/src/NumberPattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NumberPattern {
+   public static void printNumPattern(int high, int increment) {
+	   
+   }
+  
+   public static void main(String[] args) { 
+      Scanner scnr = new Scanner(System.in);
+      int num1;
+      int num2;
+      
+      num1 = scnr.nextInt();
+      num2 = scnr.nextInt();
+      scnr.close();
+      printNumPattern(num1, num2); 
+   } 
+} 
