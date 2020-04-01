db6f9c64ebc7658e0912c30b5c8ef002ad45551f
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2020-04-01 11:54:47 -0400

    Update PlantArrayListExample.java

--- Ch08/src/PlantArrayListExample.java
+++ Ch08/src/PlantArrayListExample.java
@@ -13,49 +13,66 @@ public class PlantArrayListExample {
 	//MAIN
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
-		String input;
 		ArrayList<Plant> myGarden = new ArrayList<Plant>();
+		Plant tempPlant = new Plant();
+		Flower tempFlower = new Flower();
+		
+		//Used for assigning input to objects
+		String input;
 		String plantName = "";
 		int plantCost = 0;
 		String colorOfFlowers = "";
 		boolean isAnnual; 
-		Plant tempPlant = new Plant();
-		Flower tempFlower = new Flower();
+		
 
 		//Gets input
 		input = scnr.next();
 		while(!input.equals("-1")){
 			if (input.equalsIgnoreCase("plant")) {
+				//Gets all plant fields
 				plantName = scnr.next();
 				plantCost = scnr.nextInt();
 				
+				//Assigns plant fields to temporary plant object
 				tempPlant.setPlantName(plantName);
 				tempPlant.setPlantCost(Integer.toString(plantCost));
+				
+				//Adds plant object to arraylist
 				myGarden.add(tempPlant);
 			}
 			else if (input.equalsIgnoreCase("flower")) {
+				//Gets all flower fields
 				plantName = scnr.next();
 				plantCost = scnr.nextInt();
 				isAnnual = scnr.nextBoolean();
 				colorOfFlowers = scnr.next();
 				
+				
+				//Asigns flower fields to temporary flower object 
 				tempFlower.setPlantName(plantName);
 				tempFlower.setPlantCost(Integer.toString(plantCost));
 				tempFlower.setPlantType(isAnnual);
 				tempFlower.setColorOfFlowers(colorOfFlowers);
+				
+				//Adds flower object to arraylist
 				myGarden.add(tempFlower);
 			}
 			else {
 				System.out.println("Error on input!");
 			}
+			
+			//Resets fields for next input
 			plantName = "";
 			plantCost = 0;
 			colorOfFlowers = "";
 			isAnnual = false;
 			tempPlant = new Plant();
 			tempFlower = new Flower();
+			
+			//Gets next input
 			input = scnr.next();
 		}
+		//Closes scanner and outputs all plants from arraylist
 		scnr.close();
 		printArrayList(myGarden);
 	}
