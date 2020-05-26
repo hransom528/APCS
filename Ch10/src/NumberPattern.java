664016449a05239adb4d2fd12f9fb462be8538c5
Harris Ransom <hransom528@gmail.com>
2020-05-26 10:51:17 -0400

    Added AP practice code and Ch08/Ch10 labs

--- Ch10/src/NumberPattern.java
+++ Ch10/src/NumberPattern.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 
 public class NumberPattern {
-   public static void printNumPattern(int high, int increment) {
-	   int currNum = high;
-	   
+	//MAIN
+	public static void main(String[] args) { 
+		Scanner scnr = new Scanner(System.in);
+		int num1, num2;
+		
+		num1 = scnr.nextInt();
+		num2 = scnr.nextInt();
+		printNumPattern(num1, num2); 
+		scnr.close();
 	}
-  
-   public static void main(String[] args) { 
-      Scanner scnr = new Scanner(System.in);
-      int num1;
-      int num2;
-      
-      num1 = scnr.nextInt();
-      num2 = scnr.nextInt();
-      scnr.close();
-      printNumPattern(num1, num2); 
-   } 
+
+	//Recursive method
+	public static void printNumPattern(int num, int sub) {
+		if (num <= 0) { //Base case
+			System.out.print(num + " ");
+			return; //Traverses back up the stack, recalling past recursive calls
+		}
+		else { 
+			System.out.print(num + " "); //Outputs num before
+			printNumPattern(num - sub, sub); 
+			System.out.print(num + " "); //Outputs num after
+		}
+	} 
 } 
--- Ch10/src/PhotoLineups.java
+++ Ch10/src/PhotoLineups.java
@@ -3,22 +3,47 @@ import java.util.Scanner;
 
 public class PhotoLineups {
 
-   // TODO: Write method to create and output all permutations of the list of names.
-   public static void allPermutations(ArrayList<String> permList, ArrayList<String> nameList) {
-   
-   }
+	//Creates and outputs all permutations of the list of names.
+	@SuppressWarnings("unchecked")
+	public static void allPermutations(ArrayList<String> permList, ArrayList<String> nameList) {
+		ArrayList<String> names = (ArrayList<String>) nameList.clone();
+		String tempString;
+		
+		if (names.isEmpty()) { //Base case: no more left in branch, output permute
+			for (String item : permList) {
+				System.out.print(item + " ");
+			}
+			System.out.print("\n");
+		}
+		else {
+			for (int i = 0; i < names.size(); i++) {
+				//Moves item from names to permList
+				tempString = names.remove(i); 
+				permList.add(tempString);
+				
+				//Recursively get next in list
+				allPermutations(permList, names);
+				
+				//Put item back into list, move on to next branch
+				names.add(i, tempString);
+				permList.remove(tempString);
+			}
+		}
+	}
 
-   public static void main(String[] args) {
-      Scanner scnr = new Scanner(System.in);
-      ArrayList<String> nameList = new ArrayList<String>();
-      ArrayList<String> permList = new ArrayList<String>();
-      String name;
-      
-      // TODO: Read in a list of names; stop when -1 is read. Then call recursive method.
-      while (!scnr.hasNextInt()) {
-    	  nameList.add(scnr.next());
-      }
-      
-      
-   }
+	//MAIN
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		ArrayList<String> nameList = new ArrayList<String>();
+		ArrayList<String> permList = new ArrayList<String>();
+
+		//Reads in names until number (-1) is detected
+		while (!scnr.hasNextInt()) {
+			nameList.add(scnr.next());
+		}
+		scnr.close();
+		
+		//Recursively gets all permutations of the list of names
+		allPermutations(permList, nameList);
+	}
 }
